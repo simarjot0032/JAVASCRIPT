99eac126aef0965fd3f6798b7da78db6c007bec0
simarjot <simarjot0032@gmail.com>
2024-06-04 21:16:33 +0530

    added the feature to make different platform

--- linkSharingApp/script.js
+++ linkSharingApp/script.js
@@ -7,7 +7,8 @@ let realLinkContainer = document.querySelector(".real-links-container");
 let click = 0;
 let remove = document.querySelectorAll(".link-remove-btn");
 let linkTab;
-
+let selectTag;
+let color = ["black", "#0077B5", "red", "#1877f2", "#f48024"];
 // Function Section
 function displayNone(container) {
   container.style.display = "none";
@@ -26,17 +27,25 @@ function removeInput(index) {
   }
 }
 // Fucniton for creaing link  inside the phone
-function createLinkPhone() {
+function createLinkPhone(Linkname) {
   let linkPhone = document.createElement("a");
   linkPhone.classList.add("real-link-tab");
   linkPhone.href = "#";
   linkPhone.innerHTML = `
   <div class="link-tab-content">
-  <p class="link-name">Github</p>
+  <p class="link-name">${Linkname}</p>
   <i class="fa-solid fa-arrow-right" style="color: #ffffff;"></i>
   </div>
   `;
   realLinkContainer.append(linkPhone);
+}
+function updatePlatformInMobile(platformName, index, elementindex) {
+  linkTab[index].innerHTML = `
+  
+  <div class="link-tab-content" style="background-color:${color[elementindex]}">
+  <p class="link-name" >${platformName}</p>
+  <i class="fa-solid fa-arrow-right" style="color: #ffffff;"></i>
+  </div>`;
 }
 //  Function for creaing the link in form for inputs in user
 function createLinkInputForm() {
@@ -47,20 +56,39 @@ function createLinkInputForm() {
   <p class="link-heading">= Link</p>
   <button class="link-remove-btn">Remove</button>
   </div>
+  <div class="link-input-container">
+  <label for="platform" class="label">Platform</label>
+  <select id="platform">
+  <option value="Github">Github</option>
+  <option value="LinkedIn">LinkedIn</option>
+  <option value="YouTube">YouTube</option>
+  <option value="FaceBook">FaceBook</option>
+  <option value="Stack Overflow">Stack OverFlow</option>
+  </select>
+  </div>
   </div>
   `;
+
+  // crate a link inside the phone
+  createLinkPhone("Github");
   //  adds the eventlistner in remove btn
   // Updating the value for remove variable
+  // updating the value for linkTab varibles
+  linkTab = document.querySelectorAll(".real-link-tab");
   remove = document.querySelectorAll(".link-remove-btn");
   for (let i = 0; i < remove.length; i++) {
     remove[i].addEventListener("click", () => {
       removeInput(i);
     });
   }
-  // crate a link inside the phone
-  createLinkPhone();
-  // updating the value for linkTab varibles
-  linkTab = document.querySelectorAll(".real-link-tab");
+
+  // updating the value for select tag
+  selectTag = document.querySelectorAll("#platform");
+  selectTag.forEach((element, index) => {
+    element.addEventListener("change", () => {
+      updatePlatformInMobile(element.value, index, element.selectedIndex);
+    });
+  });
 }
 
 addNewLink.addEventListener("click", () => {
