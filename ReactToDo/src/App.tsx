9b6ad55d994c376e236b71c962476826284e5858
simarjot <simarjot0032@gmail.com>
2024-07-24 10:53:57 +0530

    added scss modules setup

---
+++ ReactToDo/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./Styles/index.scss";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>React To Do using loaclstorage</>;
+}
+
+export default App;
